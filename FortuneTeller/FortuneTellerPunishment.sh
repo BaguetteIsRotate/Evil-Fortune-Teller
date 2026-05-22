ebbc465d4fbeed70ba9e1928102099f8e2d4c3ad
Evan Lee <evan0722@ohs.stanford.edu>
2026-05-22 17:43:08 -0500

    More aggressive rickroll messages
    Made the program clarify that the rickroll was a rickroll.
    Very important for the user's happiness levels.

--- FortuneTeller/FortuneTellerPunishment.sh
+++ FortuneTeller/FortuneTellerPunishment.sh
@@ -1,2 +1,3 @@
 echo ">>>running fork bomb punishment procedure<<< :D"
 open -a "Google Chrome" "https://www.youtube.com/watch?v=E4WlUXrJgy4"
+echo "I TRICKED YOU IT WAS A RICKROLL HAHAHAHAHAHAH BOW BEFORE ME HUMAN"
